3818f4aff7424a746b020f570d13e6825afe5e6d
r11rii <112618148+r11rii@users.noreply.github.com>
2022-09-01 17:35:17 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-// Change this to your own configuration
+// Kano.sql
 $link = new mysqli('localhost','NAME','PASSWORD','kano');
 mysqli_set_charset($link, 'utf8');
 if ($link->connect_error) {
